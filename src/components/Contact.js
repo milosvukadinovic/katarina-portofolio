f0ef9557de8ee28a40692e28bc5faf093d4ccc92
milosvukadinovic <milos.vukadinovic11@gmail.com>
2020-08-06 22:07:10 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,24 +5,7 @@ import pic from '../images/bg.png'
 
  const Contact = ()=>{
     return(
-        <div class="bodyContainer" style={{height:"800px"}} >
-            <img style={{height:"800px"}} src={pic} />
-            <div class="centered">
-               
-                   </div>
-                   <div class="centeredOpacity">
-                   <h1 class="titleSub">Introducing Katarina </h1>
-                   
-<div class="text-about" style={{width: "50%"}}>-2013-2017 graduated as a graphic designer at the high school "School of Design" in Belgrade </div>
-<div class="text-about" style={{width: "50%"}}>-Rewarded as the 'best and most successful graphic design student'</div>
-<div class="text-about" style={{width: "50%"}}>-Was a part of the 'Cyrillic typography' school exhibition</div>
-<div class="text-about" style={{width: "50%"}}>-Volunteered at DIMIS company as a graphic designer </div>
-<div class="text-about" style={{width: "50%"}}> -2017 started attending the "Academy of Modern arts" in Belgrade, as a graphic designer`</div>
-
-                       
-                   </div>
-      
-    </div>
+        <div> contact </div>
     )
 }
 
